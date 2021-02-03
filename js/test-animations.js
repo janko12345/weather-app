5ccf80f2ec45d7e7d9d58075598d3cee4d2f11cc
janko12 <henrich.sury97@gmail.com>
2021-02-03 23:09:17 +0100

    changed layout of astronomy info

--- js/test-animations.js
+++ js/test-animations.js
@@ -2,6 +2,12 @@ let sidebar = document.querySelector(".sidebar");
 let burger = sidebar.querySelector(".hamburger-button");
 let main = document.querySelector("main");
 let gear = main.querySelector(".gear-icon");
+let astronomyCnt = document.querySelector(".astronomy-wrapper");
+let astronomy = document.querySelector(".astronomy");
+let astronomyInfoToggler = main.querySelector(".astronomy-info-toggler");
+let astronomyVisibilityToggler = main.querySelector(
+    ".astronomy-visibility-toggler"
+);
 
 burger.addEventListener("click", (e) => {
     sidebar.classList.toggle("sidebar-show");
@@ -10,4 +16,15 @@ burger.addEventListener("click", (e) => {
 
 gear.addEventListener("click", (e) => {
     gear.classList.toggle("gear-icon-active");
+});
+
+astronomyInfoToggler.addEventListener("click", (e) => {
+    astronomyInfoToggler.classList.toggle("astronomy-info-toggler-active");
+});
+
+astronomyVisibilityToggler.addEventListener("click", (e) => {
+    astronomy.classList.toggle("astronomy-hide");
+    astronomyVisibilityToggler.classList.toggle(
+        "astronomy-visibility-toggler-rolled-up"
+    );
 });
