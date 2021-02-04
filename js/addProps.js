64fce2aefff12ca7a4fc4f9a98b09b0892d2cda7
janko12 <henrich.sury97@gmail.com>
2021-02-04 21:09:53 +0100

    added animations
    index.html - added needed elements for animation, changed astronomy class naming
    _astronomy.scss - visibility color changing and moon sun translate
    _body.scss - split github into own file cause its fixed position now
    _forecasts.scss - made dropdown menu for animation
    _header.scss - made config menu for animation
    _sidebar-header - adjusted existing styles

---
+++ js/addProps.js
@@ -0,0 +1,4 @@
+let body = document.querySelector(".body");
+let bodyHeader = main.querySelector(".header");
+
+body.style.paddingTop = `${bodyHeader.clientHeight}px`;
--- js/test-animations.js
+++ js/test-animations.js
@@ -1,17 +1,37 @@
 let sidebar = document.querySelector(".sidebar");
+let search = sidebar.querySelector(".sidebar-search");
+let fetchResponse = sidebar.querySelector(".fetch-response");
 let burger = sidebar.querySelector(".hamburger-button");
 let main = document.querySelector("main");
-let gear = main.querySelector(".gear-icon");
+let gear = main.querySelector(".config-gear-icon");
+let configMenu = main.querySelector(".config-menu");
 let astronomyCnt = document.querySelector(".astronomy-wrapper");
 let astronomy = document.querySelector(".astronomy");
 let astronomyInfoToggler = main.querySelector(".astronomy-info-toggler");
 let astronomyVisibilityToggler = main.querySelector(
     ".astronomy-visibility-toggler"
 );
-let changeInfoBtn = main.querySelector(".forecasts-buttons-change-info");
+let changeInfoBtn = main.querySelector(".forecasts-buttons-switch-img");
 let secondaryInfos = [...main.querySelectorAll(".forecast-secondary-infos")];
 let primaryInfos = [...main.querySelectorAll(".forecast-primary-infos")];
-let dropdown = main.querySelector(".forecasts-buttons-dropdown");
+let dropdown = main.querySelector(".forecasts-buttons-dropdown-img");
+let dropdownMenu = main.querySelector(".forecasts-buttons-dropdown-menu");
+
+let failedTO;
+search.addEventListener("keyup", (e) => {
+    clearTimeout(failedTO);
+    fetchResponse.classList.remove("fetch-response-failed");
+    if (e.key.toLowerCase() === "enter") {
+        fetchResponse.classList.add("fetch-response-in-process");
+        setTimeout(() => {
+            fetchResponse.classList.remove("fetch-response-in-process");
+            fetchResponse.classList.add("fetch-response-failed");
+            failedTO = setTimeout(() => {
+                fetchResponse.classList.remove("fetch-response-failed");
+            }, 3000);
+        }, 2000);
+    }
+});
 
 burger.addEventListener("click", (e) => {
     sidebar.classList.toggle("sidebar-show");
@@ -19,11 +39,13 @@ burger.addEventListener("click", (e) => {
 });
 
 gear.addEventListener("click", (e) => {
-    gear.classList.toggle("gear-icon-active");
+    gear.classList.toggle("config-gear-icon-active");
+    configMenu.classList.toggle("config-menu-active");
 });
 
 astronomyInfoToggler.addEventListener("click", (e) => {
     astronomyInfoToggler.classList.toggle("astronomy-info-toggler-active");
+    astronomyCnt.classList.toggle("astronomy-info-toggled");
 });
 
 astronomyVisibilityToggler.addEventListener("click", (e) => {
@@ -40,26 +62,37 @@ changeInfoBtn.addEventListener("click", (e) => {
     primaryInfos.forEach((primaryInfo) => primaryInfo.classList.toggle("hide"));
     if (
         changeInfoBtn.classList.contains(
-            "forecasts-buttons-change-info-rotate-180"
+            "forecasts-buttons-switch-img-rotate-180"
         )
     ) {
-        changeInfoBtn.classList.add("forecasts-buttons-change-info-rotate-360");
+        changeInfoBtn.classList.add("forecasts-buttons-switch-img-rotate-360");
         changeInfoBtn.classList.remove(
-            "forecasts-buttons-change-info-rotate-180"
+            "forecasts-buttons-switch-img-rotate-180"
         );
     } else if (
         changeInfoBtn.classList.contains(
-            "forecasts-buttons-change-info-rotate-360"
+            "forecasts-buttons-switch-img-rotate-360"
         )
     ) {
         changeInfoBtn.classList.remove(
-            "forecasts-buttons-change-info-rotate-360"
+            "forecasts-buttons-switch-img-rotate-360"
         );
-        changeInfoBtn.classList.add("forecasts-buttons-change-info-rotate-180");
+        changeInfoBtn.classList.add("forecasts-buttons-switch-img-rotate-180");
     } else
-        changeInfoBtn.classList.add("forecasts-buttons-change-info-rotate-180");
+        changeInfoBtn.classList.add("forecasts-buttons-switch-img-rotate-180");
 });
 
 dropdown.addEventListener("click", (e) => {
-    dropdown.classList.toggle("forecasts-buttons-dropdown-active");
+    dropdown.classList.toggle("forecasts-buttons-dropdown-img-active");
+    dropdownMenu.classList.toggle("forecasts-buttons-dropdown-menu-active");
+});
+
+dropdownMenu.addEventListener("click", (e) => {
+    if (e.target.classList.contains("forecasts-buttons-dropdown-item")) {
+        dropdownMenu
+            .querySelector(".forecasts-buttons-dropdown-item-active")
+            .classList.remove("forecasts-buttons-dropdown-item-active");
+        e.target.classList.add("forecasts-buttons-dropdown-item-active");
+        dropdownMenu.classList.remove("forecasts-buttons-dropdown-menu-active");
+    }
 });
